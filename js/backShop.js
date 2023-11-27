c67cc021396319461bebac20a6d883d3822b3a0c
Remilia172112 <trangianguyen.com@gmail.com>
2023-11-28 00:29:52 +0700

    fix lần 2

--- js/backShop.js
+++ js/backShop.js
@@ -1,7 +1,7 @@
 function back_shop() {
     let back_shop = document.getElementById('back_shop');
     back_shop.addEventListener('click', () => {
-        window.location.href = '../html/index.html';
+        window.location.href = '../index.html';
     });
 }
 back_shop();
